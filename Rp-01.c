22917b394bb24dbbdee0e79d0ae5d4ca1f72e26e
rathinap6 <35958265+rathinap6@users.noreply.github.com>
2018-02-27 02:46:09 -0800

    Create Rp-01.c

---
+++ Rp-01.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char s[100];
+    int len,c=0,i,t;
+    printf("Enter the String:");
+    gets(s);
+    len=strlen(s);
+    for(i=0;i<len;i++)
+     {
+        if(s[i]==' ')
+        c++;
+    }
+    t=l-c;
+    printf("%d",t);
+}
